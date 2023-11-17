187fa057c68e84c4e4c0a4d707582e92478893fc
Amine Afia <amine@blocktorch.xyz>
2023-11-17 23:32:32 +0300

    removed auth stuff

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,12 @@ export default async function IndexPage({
   searchParams: { q: string };
 }) {
   const search = searchParams.q ?? '';
-  const result = await sql`
-    SELECT id, name, username, email 
-    FROM users 
-    WHERE name ILIKE ${'%' + search + '%'};
-  `;
-  const users = result.rows as User[];
+//   const result = await sql`
+//     SELECT id, name, username, email 
+//     FROM users 
+//     WHERE name ILIKE ${'%' + search + '%'};
+//   `;
+//   const users = result.rows as User[];
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
@@ -29,7 +29,7 @@ export default async function IndexPage({
       <Text>A list of users retrieved from a Postgres database.</Text>
       <Search />
       <Card className="mt-6">
-        <UsersTable users={users} />
+        <UsersTable users={[]} />
       </Card>
     </main>
   );
