c417080669bb38852c94d7fd422fe05aad7c6793
0xtorchieG <gerald@blocktorch.xyz>
2023-11-18 18:36:53 +0300

    web3inbox functionality to register, subscribe & unsubscribe

--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -1,22 +1,16 @@
 "use client";
-import type { NextPage } from "next";
 import { useCallback, useEffect, useState } from "react";
 import React from 'react';
-import { Button, Flex, Accordion, AccordionList} from '@tremor/react';
+import { Button, Flex, Text, Accordion, AccordionList} from '@tremor/react';
 
 import {
   useInitWeb3InboxClient,
   useManageSubscription,
   useW3iAccount,
+  useSubscription
 } from "@web3inbox/widget-react";
 import "@web3inbox/widget-react/dist/compiled.css";
 import { useWeb3ModalAccount, useWeb3ModalSigner } from '@web3modal/ethers5/react'
-
-import useSendNotification from "./utils/useSendNotification";
-import Messages from "./components/Messages";
-import Subscription from "./components/Subscription";
-import { sendNotification } from "./utils/fetchNotify";
-import { truncate } from "fs/promises";
 
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as string;
 const appDomain = process.env.NEXT_PUBLIC_APP_DOMAIN as string;
@@ -34,6 +28,7 @@ export default function InboxPage() {
         setAccount,
         register: registerIdentity,
         identityKey,
+        isRegistering
     } = useW3iAccount();
     const {
         subscribe,
@@ -42,14 +37,13 @@ export default function InboxPage() {
         isSubscribing,
         isUnsubscribing,
     } = useManageSubscription(account);
+
+    // get specific account's subscription to specific dapp
+    const { subscription } = useSubscription(account, appDomain)
     
     const { signer } = useWeb3ModalSigner()
     const { address, isConnected } = useWeb3ModalAccount()
 
-    const { handleSendNotification, isSending } = useSendNotification();
-    const [lastBlock, setLastBlock] = useState<string>();
-    const [isBlockNotificationEnabled, setIsBlockNotificationEnabled] =
-        useState(true);
 
     const signMessage = useCallback(
         async (message: string) => {
@@ -70,7 +64,7 @@ export default function InboxPage() {
     useEffect(() => {
         if (!Boolean(address)) return;
         setAccount(`eip155:1:${address}`);
-    }, [signMessage, address, setAccount]);
+    }, [address, setAccount]);
 
     const handleRegistration = useCallback(async () => {
         if (!account) return;
@@ -81,33 +75,7 @@ export default function InboxPage() {
         }
     }, [signMessage, registerIdentity, account]);
 
-    useEffect(() => {
-            // register even if an identity key exists, to account for stale keys
-            handleRegistration();
-        }, [handleRegistration]);
-
-        const handleSubscribe = useCallback(async () => {
-        if(!identityKey) {
-        await handleRegistration();
-        }
-
-        await subscribe();
-    }, [subscribe, identityKey])
-
-    // handleSendNotification will send a notification to the current user and includes error handling.
-    // If you don't want to use this hook and want more flexibility, you can use sendNotification.
-    const handleTestNotification = useCallback(async () => {
-        if (isSubscribed) {
-        handleSendNotification({
-            title: "Alert",
-            body: "🧿 alert to keep you safe on chain my fren!",
-            icon: `${window.location.origin}/WalletConnect-blue.svg`,
-            url: window.location.origin,
-        // ID retrieved from explorer api - Copy your notification type from WalletConnect Cloud and replace the default value below
-            type: "fe8b4821-0e89-4b73-82ab-e9bbc484ac1a",
-        });
-        }
-    }, [handleSendNotification, isSubscribed]);
+  
 
     return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
@@ -116,41 +84,43 @@ export default function InboxPage() {
         </div>
         <div>
         <Flex flexDirection="col">
-            {isSubscribed ? (
-            <Flex flexDirection={"col"} alignItems="center">
-                <Button 
-                    onClick={handleTestNotification}
-                    disabled={!isW3iInitialized}
-                    loading={isSending}
-                    loadingText="Sending..."> 
-                    Send test notification
-                </Button>
-                <Button
-                    onClick={unsubscribe}
+        { !identityKey ? (
+                <Flex flexDirection={"col"} alignItems="center">
+                    <Text> Please register to use Nazar with Web3Inbox </Text>
+                    <Button 
+                    onClick={handleRegistration}
                     disabled={!isW3iInitialized || !account}
-                    loading={isUnsubscribing}
-                    loadingText="Unsubscribing..."
-                >
-                    Unsubscribe
+                    loading={isRegistering}
+                    loadingText="Registering..."> 
+                    Register
                 </Button>
-            </Flex>
+                </Flex>
             ) : (
-                <Button
-                    onClick={handleSubscribe}
-                    loading={isSubscribing}
-                    loadingText="Subscribing..."
-                    disabled={!Boolean(address) || !Boolean(account)}
-                >
-                    Subscribe
-                </Button>
-            )}
+                <Flex flexDirection="col">
+                    {isSubscribed ? (
+                    <Flex flexDirection={"col"} alignItems="center">
+                        <Button 
+                            onClick={unsubscribe}
+                            disabled={!isW3iInitialized || !account}
+                            loading={isUnsubscribing}
+                            loadingText="Unsubscribing..."> 
+                            Unsubscribe
+                        </Button>
+                    </Flex>
+                    ) : (
+                        <Button
+                            onClick={subscribe}
+                            disabled={!isW3iInitialized || !account}
+                            loading={isSubscribing}
+                            loadingText="Subscribing..."
+                        >
+                            Subscribe
+                        </Button>
+                    )}
+                </Flex>
+            )
 
-            {isSubscribed && (
-                <AccordionList>
-                    <Subscription />
-                    <Messages />
-                </AccordionList>
-            )}
+            }
         </Flex>
         </div>
     </main>
